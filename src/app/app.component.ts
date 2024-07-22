039a363cd48b499c9fbd2579d013822f53aa005e
Andrew <paul.rueda@hotmail.com>
2024-07-21 21:35:41 -0500

    Commit usuarios y funcionalidad con cors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListusuariosComponent } from './usuarios/listusuarios/listusuarios.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ListusuariosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/usuarios/listusuarios/listusuarios.component.ts
+++ src/app/usuarios/listusuarios/listusuarios.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listusuarios',
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './listusuarios.component.html',
   styleUrl: './listusuarios.component.css'
 })
-export class ListusuariosComponent {
+export class ListusuariosComponent implements OnInit {
+  async ngOnInit(){
+    const response = await fetch('https://accused-hedwig-sajaremastered-673fe6dd.koyeb.app/usuarios', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Error en la solicitud GET: ' + response.statusText);
+    }
+    this.usuarios = await response.json();
 
+
+  }
+  usuarios: [any] = [{}]
 }
