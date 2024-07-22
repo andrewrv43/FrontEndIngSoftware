1e93b19144ee4738c74dc309ee482cca77816325
Andrew <paul.rueda@hotmail.com>
2024-07-21 23:55:47 -0500

    rutas y cookie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ListusuariosComponent } from './usuarios/listusuarios/listusuarios.component';
 import { CrearUsuariosComponent } from './usuarios/crearusuarios/crearusuarios.component';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ListusuariosComponent,CrearUsuariosComponent],
+  imports: [RouterOutlet,ListusuariosComponent,CrearUsuariosComponent,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private cookie:CookieService) {
+  }
+  ngOnInit(): void {
+    this.cookie.get('usuario')
+  }
   title = 'FrontEndIngSoftware';
 }
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ListusuariosComponent } from './usuarios/listusuarios/listusuarios.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'listaUsuarios',
+        redirectTo: '/usuarios',
+        pathMatch: 'full'
+    },
+    {
+        path: 'usuarios',
+        component: ListusuariosComponent
+    }
+];
